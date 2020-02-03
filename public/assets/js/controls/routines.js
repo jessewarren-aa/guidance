e5ba8ff1f16b8b7c6e238a9b8b6a80b5aa46374d
Jesse <jesse.mwarren@yahoo.com>
2020-02-03 13:55:48 -0800

    Modify action icons to prepare for drag and drop

--- public/assets/js/controls/routines.js
+++ public/assets/js/controls/routines.js
@@ -5,21 +5,21 @@ export const addRoutine = (e) => {
 
   const routineElement = `
     <div class="border-bottom border-top mb-3 d-flex-center routine-creating animated bounceInDown faster">
-      <div class="no-padding-x display-font add-reward-icon-div border-right clickable">
-        reward
+      <div class="margin-r-10 no-padding-x display-font add-reward-icon-div border-right clickable">
         <img class="icon-sizer-ui add-reward-icon img-fluid" src="assets/images/ui/gems.svg" />
       </div>
 
-      <div class="no-padding-x display-font plus-icon-div clickable">
-        <img class="icon-sizer-ui plus-icon img-fluid" src="assets/images/ui/plus.svg" />
-        action
-      </div>
+      <div class="no-padding-x grey-icon-div"></div>
+      <div class="no-padding-x grey-icon-div"></div>
+      <div class="no-padding-x grey-icon-div"></div>
+      <div class="no-padding-x grey-icon-div"></div>
+      <div class="no-padding-x grey-icon-div"></div>
     </div>
   `
 
   $(targetElement).before(routineElement)
 
-  animationBind("plus")
+  animationBind("plus-grey")
   animationBind("add-reward")
 
   // $(targetElement).addClass("routine-creating")
