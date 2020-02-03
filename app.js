5bb431fb95b3764609b4b9c6ddca2b6938fb6826
Jesse <jesse.mwarren@yahoo.com>
2020-02-02 17:49:00 -0800

    Prep for heroku deployment

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running on port ${port}/`);
 });
