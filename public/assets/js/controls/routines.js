21318a2f3b8ea99c83697bc18bac80238c018054
Jesse <jesse.mwarren@yahoo.com>
2020-02-04 09:30:22 -0800

    Add routine population from localStorage

--- public/assets/js/controls/routines.js
+++ public/assets/js/controls/routines.js
@@ -59,14 +59,112 @@ export const addRoutine = (e) => {
   // $(targetElement).addClass("routine-creating")
 }
 
+const loadRoutine = (timeStamp) => {
+  const targetElement = $(".new-routine")
+
+  const localStorageObject = JSON.parse(window.localStorage.getItem(`routine-${timeStamp}`))
+  const drop1 = localStorageObject["drop-1-" + timeStamp] || ""
+  const drop1Items = drop1.split(":SEPARATOR:")
+  const drop1Content = `<img 
+        id="dragReading" 
+        draggable="true" ondragstart="dragStart(event)"
+        class="${drop1Items[0]}" src="${drop1Items[1]}" 
+        />`
+
+  const drop2 = localStorageObject["drop-2-" + timeStamp] || ""
+  const drop2Items = drop2.split(":SEPARATOR:")
+  const drop2Content = `<img 
+        id="dragReading" 
+        draggable="true" ondragstart="dragStart(event)"
+        class="${drop2Items[0]}" src="${drop2Items[1]}" 
+        />`
+
+  const drop3 = localStorageObject["drop-3-" + timeStamp] || ""
+  const drop3Items = drop3.split(":SEPARATOR:")
+  const drop3Content = `<img 
+        id="dragReading" 
+        draggable="true" ondragstart="dragStart(event)"
+        class="${drop3Items[0]}" src="${drop3Items[1]}" 
+        />`
+
+  const drop4 = localStorageObject["drop-4-" + timeStamp] || ""
+  const drop4Items = drop4.split(":SEPARATOR:")
+  const drop4Content = `<img 
+        id="dragReading" 
+        draggable="true" ondragstart="dragStart(event)"
+        class="${drop4Items[0]}" src="${drop4Items[1]}" 
+        />`
+
+  const drop5 = localStorageObject["drop-5-" + timeStamp] || ""
+  const drop5Items = drop5.split(":SEPARATOR:")
+  const drop5Content = `<img 
+        id="dragReading" 
+        draggable="true" ondragstart="dragStart(event)"
+        class="${drop5Items[0]}" src="${drop5Items[1]}" 
+        />`
+  
+  const routineElement = `
+    <div 
+      id="routine-${timeStamp}"
+      class="border-bottom border-top mb-3 d-flex-center routine-creating animated bounceInDown faster"
+    >
+      <div class="margin-r-10 no-padding-x display-font add-reward-icon-div border-right clickable">
+        <img class="icon-sizer-ui add-reward-icon img-fluid" src="assets/images/ui/gems.svg" />
+      </div>
+
+      <div 
+        id="drop-1-${timeStamp}"
+        class="no-padding ${drop1 === "" ? "" : "remove-border"} grey-icon-div" ondragover="allowDrop(event)" ondrop="dragDrop(event)"
+      >${drop1 === "" ? "" : drop1Content}</div>
+      <div 
+        id="drop-2-${timeStamp}"
+        class="no-padding ${drop2 === "" ? "" : "remove-border"} grey-icon-div" ondragover="allowDrop(event)" ondrop="dragDrop(event)"
+      >${drop2 === "" ? "" : drop2Content}</div>
+      <div 
+        id="drop-3-${timeStamp}"
+        class="no-padding ${drop3 === "" ? "" : "remove-border"} grey-icon-div" ondragover="allowDrop(event)" ondrop="dragDrop(event)"
+      >${drop3 === "" ? "" : drop3Content}</div>
+      <div 
+        id="drop-4-${timeStamp}"
+        class="no-padding ${drop4 === "" ? "" : "remove-border"} grey-icon-div" ondragover="allowDrop(event)" ondrop="dragDrop(event)"
+      >${drop4 === "" ? "" : drop4Content}</div>
+      <div 
+        id="drop-5-${timeStamp}"
+        class="no-padding ${drop5 === "" ? "" : "remove-border"} grey-icon-div" ondragover="allowDrop(event)" ondrop="dragDrop(event)"
+      >${drop5 === "" ? "" : drop5Content}</div>
+      <div 
+        class="margin-l-10 no-padding-x display-font cancel-icon-div border-left clickable"
+        onclick="destroyRoutine(event)"
+      >
+        <img class="icon-sizer-ui cancel-icon img-fluid" src="assets/images/ui/cancel.svg" />
+      </div>
+    </div>
+  `
+
+  $(targetElement).before(routineElement)
+  animationBind("add-reward")
+}
+
 const destroyRoutine = (e) => {
   const routineId = $(e.target).parent().parent().attr('id')
   window.localStorage.removeItem(routineId)
   $(e.target).parent().parent().remove()
 }
 
+const generateLocalStorageRoutines = () => {
+  Object.keys(localStorage).forEach(localStorageKey => {
+    if (localStorageKey.toString().startsWith("routine-")) {
+      loadRoutine(localStorageKey.split("-")[1])
+    } // else, ignored key
+  })
+
+  // $(beforeThisElement).before
+}
+
 window.addRoutine = addRoutine
 window.destroyRoutine = destroyRoutine
+
+generateLocalStorageRoutines()
 
 export default {
   addRoutine,
