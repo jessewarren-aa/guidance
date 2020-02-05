5336d117a81118a5b15789aab4b75bbe03c6fb93
Jesse <jesse.mwarren@yahoo.com>
2020-02-05 08:36:18 -0800

    Load routines on usage page without drag/drop mechanics

--- public/assets/js/controls/routines.js
+++ public/assets/js/controls/routines.js
@@ -66,7 +66,7 @@ const loadRoutine = (timeStamp) => {
   const drop1 = localStorageObject["drop-1-" + timeStamp] || ""
   const drop1Items = drop1.split(":SEPARATOR:")
   const drop1Content = `<img 
-        id="dragReading" 
+        id="${drop1Items[2]}" 
         draggable="true" ondragstart="dragStart(event)"
         class="${drop1Items[0]}" src="${drop1Items[1]}" 
         />`
@@ -74,7 +74,7 @@ const loadRoutine = (timeStamp) => {
   const drop2 = localStorageObject["drop-2-" + timeStamp] || ""
   const drop2Items = drop2.split(":SEPARATOR:")
   const drop2Content = `<img 
-        id="dragReading" 
+        id="${drop2Items[2]}" 
         draggable="true" ondragstart="dragStart(event)"
         class="${drop2Items[0]}" src="${drop2Items[1]}" 
         />`
@@ -82,7 +82,7 @@ const loadRoutine = (timeStamp) => {
   const drop3 = localStorageObject["drop-3-" + timeStamp] || ""
   const drop3Items = drop3.split(":SEPARATOR:")
   const drop3Content = `<img 
-        id="dragReading" 
+        id="${drop3Items[2]}" 
         draggable="true" ondragstart="dragStart(event)"
         class="${drop3Items[0]}" src="${drop3Items[1]}" 
         />`
@@ -90,7 +90,7 @@ const loadRoutine = (timeStamp) => {
   const drop4 = localStorageObject["drop-4-" + timeStamp] || ""
   const drop4Items = drop4.split(":SEPARATOR:")
   const drop4Content = `<img 
-        id="dragReading" 
+        id="${drop4Items[2]}" 
         draggable="true" ondragstart="dragStart(event)"
         class="${drop4Items[0]}" src="${drop4Items[1]}" 
         />`
@@ -98,7 +98,7 @@ const loadRoutine = (timeStamp) => {
   const drop5 = localStorageObject["drop-5-" + timeStamp] || ""
   const drop5Items = drop5.split(":SEPARATOR:")
   const drop5Content = `<img 
-        id="dragReading" 
+        id="${drop5Items[2]}" 
         draggable="true" ondragstart="dragStart(event)"
         class="${drop5Items[0]}" src="${drop5Items[1]}" 
         />`
@@ -149,6 +149,54 @@ const loadRoutine = (timeStamp) => {
   animationBind("add-reward")
 }
 
+const loadUsedRoutine = (timeStamp) => {
+  const targetElement = $(".use-page-placer")
+
+  const localStorageObject = JSON.parse(window.localStorage.getItem(`routine-${timeStamp}`))
+  const drop1 = localStorageObject["drop-1-" + timeStamp] || ""
+  const drop1Items = drop1.split(":SEPARATOR:")
+  const drop1Content = `<img id="${drop1Items[2]}" class="${drop1Items[0]}" src="${drop1Items[1]}" />`
+
+  const drop2 = localStorageObject["drop-2-" + timeStamp] || ""
+  const drop2Items = drop2.split(":SEPARATOR:")
+  const drop2Content = `<img id="${drop2Items[2]}" class="${drop2Items[0]}" src="${drop2Items[1]}" />`
+
+  const drop3 = localStorageObject["drop-3-" + timeStamp] || ""
+  const drop3Items = drop3.split(":SEPARATOR:")
+  const drop3Content = `<img id="${drop3Items[2]}" class="${drop3Items[0]}" src="${drop3Items[1]}" />`
+
+  const drop4 = localStorageObject["drop-4-" + timeStamp] || ""
+  const drop4Items = drop4.split(":SEPARATOR:")
+  const drop4Content = `<img id="${drop4Items[2]}" class="${drop4Items[0]}" src="${drop4Items[1]}" />`
+
+  const drop5 = localStorageObject["drop-5-" + timeStamp] || ""
+  const drop5Items = drop5.split(":SEPARATOR:")
+  const drop5Content = `<img id="${drop5Items[2]}" class="${drop5Items[0]}" src="${drop5Items[1]}" />`
+
+  const routineElement = `
+    <div 
+      id="use-routine-${timeStamp}"
+      class="border-bottom border-top mb-3 d-flex-center routine-creating animated bounceInDown faster"
+    >
+      <div 
+        id="use-reward-${timeStamp}"
+        class="margin-r-10 no-padding-x display-font add-reward-icon-div border-right clickable"
+      >
+        <img class="icon-sizer-ui add-reward-icon img-fluid" src="assets/images/ui/gems.svg" />
+      </div>
+
+      <div class="no-padding ${drop1 === "" ? "" : "remove-border"} grey-icon-div">${drop1 === "" ? "" : drop1Content}</div>
+      <div class="no-padding ${drop2 === "" ? "" : "remove-border"} grey-icon-div" >${drop2 === "" ? "" : drop2Content}</div>
+      <div class="no-padding ${drop3 === "" ? "" : "remove-border"} grey-icon-div" >${drop3 === "" ? "" : drop3Content}</div>
+      <div class="no-padding ${drop4 === "" ? "" : "remove-border"} grey-icon-div" >${drop4 === "" ? "" : drop4Content}</div>
+      <div class="no-padding ${drop5 === "" ? "" : "remove-border"} grey-icon-div" >${drop5 === "" ? "" : drop5Content}</div>
+    </div>
+  `
+
+  $(targetElement).before(routineElement)
+  animationBind("add-reward")
+}
+
 const destroyRoutine = (e) => {
   const routineId = $(e.target).parent().parent().attr('id')
   window.localStorage.removeItem(routineId)
@@ -169,6 +217,7 @@ const generateLocalStorageRoutines = () => {
   Object.keys(localStorage).forEach(localStorageKey => {
     if (localStorageKey.toString().startsWith("routine-")) {
       loadRoutine(localStorageKey.split("-")[1])
+      loadUsedRoutine(localStorageKey.split("-")[1])
     } // else, ignored key
   })
 }
