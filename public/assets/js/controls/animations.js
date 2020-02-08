e565e812bb66160359079c0f60af8c70b97c9335
Jesse <jesse.mwarren@yahoo.com>
2020-02-07 16:01:08 -0800

    Animate about page emojis

--- public/assets/js/controls/animations.js
+++ public/assets/js/controls/animations.js
@@ -13,3 +13,54 @@ animationBind("create")
 animationBind("settings")
 animationBind("back")
 
+const animationCycle = () => {
+  const elements = [ // reference
+    "#pine-thinking",
+    "#pine-chemist",
+    "#pine-credits",
+    "#pine-hearts"
+  ]
+
+  $("#pine-thinking").bind(
+    "webkitAnimationEnd mozAnimationEnd animationend",
+    () => {
+      $("#pine-thinking").removeClass("rubberBand");
+      setTimeout(() => {
+        $("#pine-chemist").addClass("shake")
+      }, 6800)
+    }
+  );
+
+  $("#pine-chemist").bind(
+    "webkitAnimationEnd mozAnimationEnd animationend",
+    () => {
+      $("#pine-chemist").removeClass("shake");
+      setTimeout(() => {
+        $("#pine-credits").addClass("swing");
+      }, 3200);
+    }
+  );
+
+  $("#pine-credits").bind(
+    "webkitAnimationEnd mozAnimationEnd animationend",
+    () => {
+      $("#pine-credits").removeClass("swing");
+      setTimeout(() => {
+        $("#pine-hearts").addClass("heartBeat");
+      }, 3600);
+    }
+  );
+
+  $("#pine-hearts").bind(
+    "webkitAnimationEnd mozAnimationEnd animationend",
+    () => {
+      $("#pine-hearts").removeClass("heartBeat");
+      setTimeout(() => {
+        $("#pine-thinking").addClass("rubberBand");
+      }, 2800);
+    }
+  );
+}
+
+animationCycle()
+$("#pine-thinking").addClass("rubberBand");
